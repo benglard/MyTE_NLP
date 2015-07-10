76a7373d2c026ea5c4058ee50809716d40efcb1c
Benjamin Englard <benjienglard@yahoo.com>
2015-07-10 10:41:39 -0700

    trainer:predict

--- MyTE_NLP/RL/Trainer.lua
+++ MyTE_NLP/RL/Trainer.lua
@@ -3,14 +3,14 @@ local RLTrainer = torch.class('rl.RLTrainer')
 function RLTrainer:__init(agent, stateFunc, rewardFunc)
    --[[
       REQUIRES:
-      agent -> a reinforcement learning agent
-      stateFunc -> function with no inputs, which
-         computes and returns the current state
-         of the agent
-      rewardFunc -> a function with one input
-         representing the action the agent has just
-         taken, which computes and returns the
-         reward of that action (a number).
+         agent -> a reinforcement learning agent
+         stateFunc -> function with no inputs, which
+            computes and returns the current state
+            of the agent
+         rewardFunc -> a function with one input
+            representing the action the agent has just
+            taken, which computes and returns the
+            reward of that action (a number).
       EFFECTS:
          Creates an instance of rl.RLTrainer for
          use in training reinforcement learning
@@ -61,4 +61,38 @@ function RLTrainer:train(params)
          end
       end
    end
+end
+
+function RLTrainer:predict(input, params)
+   --[[
+      REQUIRES:
+         input -> input to self.agent.network.forward
+         params -> set of parameters
+      EFFECTS:
+         Returns the predicted action for
+         a specific input
+   ]]
+
+   params = params or {}
+   local sample = params.sample or false
+   local temp = params.temp or 0.1
+   local length = params.length or 1
+
+   local rv = {input}
+   for n = 1, length do
+      local t = torch.zeros(self.agent.nstates)
+      t[input] = 1.0
+      local o = self.agent.network:forward(t):clone()
+
+      if sample then
+        local probs = torch.exp(o:div(temp))
+        probs:div(probs:sum(1):squeeze())
+        input = torch.multinomial(probs:float(), 1):squeeze()
+      else
+         local m, am = o:max(1)
+         input = am:squeeze()
+      end
+      table.insert(rv, input)
+   end
+   return rv
 end
